41630978b15a9c2be5820155ad4ecde6d4a97f96
ananaer <605711567@qq.com>
2026-02-19 18:55:15 +0800

    Updated index.html

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         admin: resolve(__dirname, "admin.html"),
+        policies: resolve(__dirname, "policies.html"),
       },
     },
   },
